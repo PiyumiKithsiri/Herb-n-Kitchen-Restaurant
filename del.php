3758c3ce9156be3676701ea6a5eba3d4e0cc336b
PiyumiKithsiri <149657739+PiyumiKithsiri@users.noreply.github.com>
2024-01-28 20:32:24 +0530

    Add files via upload

---
+++ del.php
@@ -0,0 +1,32 @@
+<?php
+require_once("connection.php");
+
+if (isset($_POST['del'])) {
+    $delname = mysqli_real_escape_string($conn, $_POST['delname']);
+    $delphone = mysqli_real_escape_string($conn, $_POST['delphone']);
+    $deladdress = mysqli_real_escape_string($conn, $_POST['deladdress']);
+    $delcity = mysqli_real_escape_string($conn, $_POST['delcity']);
+
+    // Insert data into the 'delivery' table
+    $query = "INSERT INTO delivery (name, phone_number, del_address, del_city, del_date, del_time)
+              VALUES ('$delname', '$delphone', '$deladdress', '$delcity', NOW(), NOW())";
+
+    if (mysqli_query($conn, $query)) {
+        // Data insertion successful
+        echo "
+        <script>
+            alert('Details Received.');
+            window.location.href='payment.php'; // Redirect to the next page
+        </script>
+        ";
+    } else {
+        // Data insertion failed
+        echo "
+        <script>
+            alert('Cannot Run Query.');
+            window.location.href='delivery.php'; // Redirect back to the form page
+        </script>
+        ";
+    }
+}
+?>
