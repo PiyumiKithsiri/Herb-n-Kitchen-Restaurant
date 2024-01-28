896907c2b6c63749f95138250f02f75133fed753
PiyumiKithsiri <149657739+PiyumiKithsiri@users.noreply.github.com>
2024-01-28 20:31:34 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php
+
+$conn = mysqli_connect("localhost","root","","restaurant");
+
+if (!$conn) 
+{
+    echo"Connection is not Successful!";
+}
+?>
---
+++ contact.php
@@ -0,0 +1,469 @@
+<html>
+<head>
+	<title> Herb 'n Kitchen </title>
+	
+<style>
+	body{
+		background-color: #3a3f43;
+        body {font-family: Arial, Helvetica, sans-serif;}
+        * {box-sizing: border-box;}
+	}
+	*{
+	margin:auto;
+	padding:auto;
+	}
+	.top{
+        width:100%;
+        background-color:#090c0e !important;
+        border-bottom:9px solid #e5c10b;
+    }
+    .top div{
+        width:1200px;
+        color:#fff;
+        background-color:#090c0e !important;
+        font-family:calibri;
+        padding:15px;
+        text-align:right;
+    }
+    
+    .logo{
+        width:1500px;
+        padding:10px;
+        background-color:#430202;
+        border-bottom:2px solid #000;
+    }
+    .logo div{
+        width:1300px;
+        background-color:#430202;
+        font-family:calibri;
+        padding:10px;
+        text-align:left;
+    }
+    .logo div table a{
+        padding:10px;
+        text-decoration:none;
+        color:gray;
+    }
+    .logo div table a:hover{
+        color:#e5c10b;
+        border-bottom:2px solid rgb(251, 246, 246);
+    }
+    /*background image*/
+    .bg {
+        /* The image used */
+        background-image: url("18.jpg");
+      
+        /* Full height */
+        height: 100%; 
+      
+        /* Center and scale the image nicely */
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+      }
+      /*background image over*/
+
+
+      /*button and text*/
+      .hero-text {
+        text-align: center;
+        position: absolute;
+        top: 80%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        color: #eeeec0;
+      }
+      
+      .hero-text button {
+        border: none;
+        outline: 0;
+        display: inline-block;
+        padding: 10px 25px;
+        color: black;
+        background-color: #b82e2e;
+        text-align: center;
+        cursor: pointer;
+      }
+      
+      .hero-text button:hover {
+        background-color: #e3dada;
+        color: rgb(117, 14, 14);
+      }
+      /*button and text over*/
+
+
+
+      /*text on the image
+      .centered {
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        padding-top: 200px;
+        font-family: Arial;
+      }*/
+      /*text on the image over*/
+
+    /*w3school contact form*/
+    input[type=text], select, textarea {
+    width: 100%;
+    padding: 12px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+    margin-top: 6px;
+    margin-bottom: 16px;
+    resize: vertical;
+    }
+
+    input[type=submit] {
+    background-color: #04AA6D;
+    color: white;
+    padding: 12px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    }
+
+    input[type=submit]:hover {
+    background-color: #45a049;
+    }
+
+    .container {
+    border-radius: 5px;
+    background-color: #f2f2f2;
+    padding: 20px;
+    }
+    /*contact form over*/
+
+    .slider_section .carousel-indicators {
+        margin-top: 45px;
+      }
+      
+    
+    .middle{
+        width:100%;
+        background-color:#000;
+        border-bottom:2px solid #000;
+        background-repeat: no-repeat;
+    }
+    .middle div{
+        width:1200px;
+        height:400px;
+        font-family:calibri;
+    }
+    .middle div p{
+        width:1000px;
+        font-weight:bold;
+        height:300px;
+        color:#000;
+        opacity:0.2;
+        font-size:50px;
+        background-color:#000;
+    }
+    
+    .bottom{
+        width:100%;
+        background-color:#000;
+        border-bottom:2px solid #1e1a1a;
+    }
+    .bottom div{
+        width:1200px;
+        color:#000;
+        background-color:#000;
+        font-family:calibri;
+        text-align:right;
+    }
+    .bottom div table{
+        padding-top:3px;
+        width:1100px;
+        text-align:justify;
+        color:gray;
+        font-size:20px;
+    }
+    
+    .bottom_up{
+        width:100%;
+        /*background-color:;*/
+        border-bottom:20px solid #000;
+    }
+    .bottom_up div{
+        padding-top:20px;
+        width:1200px;
+        text-align:center;
+        /*background-color:;*/
+        font-family:forte;
+        font-size:30px;
+        padding-bottom:20px;
+    }
+    .bottom_up div table{
+        padding-top:30px;
+        font-size:30px;
+        width:1100px;
+        text-align:center;
+        background-color:#000;
+        font-family:calibri;
+        
+    }
+    
+    input[type="text"]{
+        padding:12px;
+        width:250px;
+    }
+    select{
+        padding:12px;
+        width:250px;
+    }
+    textarea{
+        padding:50px;
+        width:550px;
+    }
+    button{
+        width:550px;
+        padding:10px;
+        color:#fff;
+        font-size:20px;
+        background-color:#e5c10b  !important;
+    }
+    
+
+    header{
+      position: fixed;
+      z-index: 1;
+      display: ;
+
+    }
+    
+    .nav_down{
+        width:100%;
+        background-color:#090c0e !important;
+        border-bottom:2px solid #f0f0f0;
+    }
+    .nav_down div{
+        width:1200px;
+        color:#e5c10b;
+        background-color:#000;
+        font-family:calibri;
+        padding:10px;
+        text-align:left;
+        text-align:center;
+    }
+
+.mySlides {
+    display:auto;
+}
+
+img.mySlides{
+    width : 900px;
+    height: 450px;
+    opacity: 0.8;
+	
+}
+
+.mySlides {
+    display:auto;
+}
+
+.collphoto{
+    max-width:500px;
+    display: block;
+    margin-left: 250px;
+    
+}
+
+.but{
+    width: 80px;
+    height: 40px;
+    border-radius: 10px;
+    color: #000;
+    float: right;
+    text-align: center; 
+    background-color:#e5c10b  !important; 
+    font-size:20px; 
+}
+
+</style>
+	
+</head>
+<body>
+<header class="header">
+	<div class="top">
+		<div>
+			Contact Us +94 47 2237748 / 01 | Herb_Kitchen.com 
+		</div>
+	</div>
+	
+	<div class="logo">
+		<div>
+			<table>
+				<tr>
+					<td width="600px" style="font-size:50px;font-family:forte;"> <font color="#e5c10b"> Herb 'n   </font><font color="#e5c10b"> Kitchen</font> </td>
+					<td> <br> <br>
+						<font size="4px" style="font-family:forte;"> 
+							<a href="Home.php">HOME</a> 
+							<a href="newabout.php">ABOUT US</a>  
+							<a href="menu.php">MENU</a>
+							<a href="contact.php">CONTACT US</a>
+                            <a href="newcart.php">CART</a>    
+                            <a href="sing_up.php"><button class="but" style="color:#000;">Logout</button></a> 
+						</font>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</div>
+</header>
+<br><br><br><br><br><br>
+    <!--text on the image-->
+    <div class="bg">
+        <!--butoon and text-->
+        <div class="hero-text">
+            <h1 style="font-size:50px">Contact Us</h1><br>
+            <p style="font-size:40px">WE WELCOME ANY QUESTION YOU MAY HAVE</p>
+            <button>OUR MENU</button>
+                    <!--butoon and text over-->
+          </div>
+        <!--<div class="centered">
+            
+                <p style="color:rgb(240, 234, 234); font-size: 150px; font-family:;">Contact Us</p>
+                <p style="color:#f0f0f0; font-size:30px; ">WE WELCOME ANY QUESTION YOU MAY HAVE</p>
+            
+        </div>-->
+    </div>
+    <!--text on the image over-->
+
+    <!--contact information-->
+    <div class="bottom">
+		<div>
+			<table border="0">
+				<tr>
+					<td width="700px">
+						<font color="#000">------------ </font> <br> <br>
+
+					<font color="white" size="5px"><br> CONTACT US </font> <br> <br>
+					Herb 'n Kitchen 
+					<br>
+					<br>
+					Mob :  +94 47 2237748
+					<br>
+					<br>
+					Email : Herb_Kitchen.com
+                    <br>
+                    <br>
+                    Address : 23/128,Palliyawaththa,<br>
+                              Piliwaana Road,<br>
+                              Tissamaharama.<br>
+                    <br>
+                    <br>
+                    <br>
+                    </td>
+					<td style="padding-left:20px;">
+                        <img src="20.jpg" width="450px" height="250px">    
+                    </td>
+				</tr>
+			</table>
+		</div>
+	</div>
+    <!--w3 school contact form-->
+    <br>
+    <h3>Contact Form</h3>
+
+    <div class="container">
+    <form action="/action_page.php">
+        <label for="fname">First Name</label>
+        <input type="text" id="fname" name="firstname" placeholder="Your name.."><br>
+
+        <label for="lname">Last Name</label>
+        <input type="text" id="lname" name="lastname" placeholder="Your last name.."><br>
+
+        <label for="country">Country</label>
+        <select id="country" name="country">
+        <option value="australia">Australia</option>
+        <option value="canada">Canada</option>
+        <option value="usa">USA</option>
+        <option value="usa">USA</option>
+        </select><br>
+
+        <label for="subject">Subject</label>
+        <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea><br>
+
+        <input type="submit" value="Submit">
+    </form>
+    </div>
+
+    
+    <!--iframe image-->
+	<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31883.157512817135!2d79.87131535440696!3d6.943082350095535!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae259b6c0e023d7%3A0x5089bb4aa922570c!2sUrban%20Kitchen!5e0!3m2!1sen!2slk!4v1701934974151!5m2!1sen!2slk" width="100%" height="300px" style="border:0;" name="iframe_a" title="Iframe Example" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+	<td style="padding-left:20px;"> </td>
+	<br>
+	<br>
+
+<!--functionality-->
+<div class="bottom">
+		<div>
+			<table border="0">
+				<tr>
+                    <td>
+                        <font color="white" size="5px">|The<br> traditionally <br>Sri Lankan, dining <br>experience.  </font> <br> <br>
+                    </td>
+                    
+					<td >
+                        <font color="white" size="5px">Herb 'n Kitchen </font> <br> <br>
+                        <a href="Home.php" style="text-decoration:none; font-weight:bold; color:gray;">Herb 'n Kitchen </a>
+					    <br>
+					    <a href="Home.php" style="text-decoration:none; font-weight:bold; color:gray;">Book</a>
+					    <br>
+					    <a href="contact.php" style="text-decoration:none; font-weight:bold; color:gray;">Location</a>
+                        <br>
+                        <a href="menu.php" style="text-decoration:none; font-weight:bold; color:gray;">Menu</a>
+                        <br>
+                    </td>
+				
+					<td ><br>
+                        <font color="white" size="5px">Stay Connected </font> <br> <br>
+                        <a href="contact.php" style="text-decoration:none; font-weight:bold; color:gray;">Contact Us</a>
+                        <br>
+                        <a href="contact.php" style="text-decoration:none; font-weight:bold; color:gray;">Fecebook</a>
+                        <br>
+                        <a href="https://www.instagram.com/accounts/login/" target="blank" style="text-decoration:none; font-weight:bold; color:gray;">Instagram</a>
+                        <br>
+                        <a href="https://myaccount.google.com/?utm_source=sign_in_no_continue&pli=1" target="blank" style="text-decoration:none; font-weight:bold; color:gray;">Email Us</a>
+                        <br>
+                        <a href="newabout.php" style="text-decoration:none; font-weight:bold; color:gray;">Write to Us</a>
+                        <br>
+                        <br>
+                    </td>
+
+                    <td style="padding-left:20px;">
+                        <font color="white" size="5px">More</font> <br> <br>
+                        
+                        Gift Vouchers
+                        <br>
+                        Two For One T&C's
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                    </td>
+
+                    <td style="padding-left:20px;">
+                        <font color="white" size="5px">Info</font> <br> <br>
+                        Careers
+                        <br>
+                        Privacy Policy
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                    </td>
+				</tr>
+			</table>
+		</div>
+	</div>
+
+	<div class="nav_down">
+		<div>
+		  Herb 'n Kitchen &copy; 2023|Aii rights reserved<br> Website Designed & Powered by P & I Creative
+		</div>
+	</div>
+	
+
